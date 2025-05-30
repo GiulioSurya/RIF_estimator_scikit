8d1bc78b4ba6d1fe01bfbcab3d97811b96341d5e
GiulioSurya <giuliosurya.loverde@studio.unibo.it>
2025-05-30 10:32:23 +0200

    added estimator, git ignore and requirements.txt

--- rif_estimator/RIF.py
+++ rif_estimator/RIF.py
@@ -1,6 +1,7 @@
+import numpy as np
 from sklearn.utils.validation import check_is_fitted
 from skopt.space import Integer
-from residual_gen import ResidualGenerator
+from rif_estimator.residual_gen import ResidualGenerator
 import pandas as pd
 from sklearn.base import BaseEstimator, OutlierMixin
 from sklearn.ensemble import IsolationForest
@@ -18,7 +19,7 @@ class ResidualIsolationForest(BaseEstimator, OutlierMixin):
             contamination: float = 0.10,
             residual_strategy: str = "oob",
             bayes_search: bool = False,
-            bayes_iter: int = 10,
+            bayes_iter: int = 3,
             bayes_cv: int = 3,
             rf_search_space: Optional[Dict[str, Integer]] = None,
             rf_params: Optional[Dict] = None,
@@ -57,3 +58,56 @@ class ResidualIsolationForest(BaseEstimator, OutlierMixin):
         check_is_fitted(self, "if_")
         res = self.generator.transform(X)
         return self.if_.predict(res)
+
+
+
+if __name__ == "__main__":
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score, recall_score, confusion_matrix
+
+    df = pd.read_csv(r"C:\Users\loverdegiulio\PycharmProjects\tesi\datas\synthetic_data_testX.csv")
+
+    ENV_COLS = ["env_X0", "env_X1", "env_X2", "env_X3", "env_X4", "env_X5"]
+    IND_COLS = ["ind_Y0", "ind_Y1", "ind_Y2"]
+
+    y = df["is_anomaly"].to_numpy()
+    X = df.drop(columns=["is_anomaly", "is_outlier"])
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.30,
+        random_state=42,
+    )
+
+    rif = ResidualIsolationForest(
+        ind_cols=IND_COLS,
+        env_cols=ENV_COLS,
+        contamination=0.20,
+        random_state=42,
+        residual_strategy="oob",
+        bayes_search=True,
+        iso_params={"max_features": 1}
+    )
+    rif.fit(X_train)
+
+    y_pred_test = np.where(rif.predict(X_test) == -1, 1, 0)
+
+    acc_rif = accuracy_score(y_test, y_pred_test)
+    rec_rif = recall_score(y_test, y_pred_test)
+    cm_rif = confusion_matrix(y_test, y_pred_test)
+
+    print(f"Accuracy : {acc_rif:.3f}")
+    print(f"Recall   : {rec_rif:.3f}")
+    print("Confusion matrix:\n", cm_rif)
+
+    iso = IsolationForest(contamination=0.20, random_state=42, max_features=1).fit(X_train)
+    iso_pred_test = np.where(iso.predict(X_test) == -1, 1, 0)
+
+    acc_iso = accuracy_score(y_test, iso_pred_test)
+    rec_iso = recall_score(y_test, iso_pred_test)
+    cm_iso = confusion_matrix(y_test, iso_pred_test)
+
+    print(f"Accuracy : {acc_iso:.3f}")
+    print(f"Recall   : {rec_iso:.3f}")
+    print("Confusion matrix:\n", cm_iso)
+
---
+++ rif_estimator/__init__.py
@@ -0,0 +1,4 @@
+from .residual_gen import ResidualGenerator
+from .RIF import ResidualIsolationForest
+
+__all__ = ["ResidualGenerator", "ResidualIsolationForest"]
