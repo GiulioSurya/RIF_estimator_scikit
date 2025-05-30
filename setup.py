541df0987cd7279faca10ef0d208861ebd645387
GiulioSurya <giuliosurya.loverde@studio.unibo.it>
2025-05-30 10:42:42 +0200

    added estimator, git ignore and requirements.txt

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# Legge il file requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='rif_estimator',
+    version='0.1.0',
+    description='Scikit-learn compatible estimator for contextual anomaly detection based on residuals and Isolation Forest.',
+    author='Giulio Surya Lo Verde',
+    url='https://github.com/GiulioSurya/RIF_estimator_scikit',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Scikit-Learn',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
